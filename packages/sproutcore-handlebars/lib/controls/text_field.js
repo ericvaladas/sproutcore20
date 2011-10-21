7db028dd8cd1fe56a478363ba9bd2d9f75ca6bdc
Eric Valadas <ericvaladas@komet.local>
2011-10-21 15:34:45 -0400

    Added maxlength property to attributeBindings in SC.TextField.

--- packages/sproutcore-handlebars/lib/controls/text_field.js
+++ packages/sproutcore-handlebars/lib/controls/text_field.js
@@ -19,10 +19,11 @@ SC.TextField = SC.View.extend(
   cancel: SC.K,
 
   tagName: "input",
-  attributeBindings: ['type', 'placeholder', 'value'],
+  attributeBindings: ['type', 'placeholder', 'value', 'maxlength'],
   type: "text",
   value: "",
   placeholder: null,
+  maxlength: null,
 
   focusOut: function(event) {
     this._elementValueDidChange();
